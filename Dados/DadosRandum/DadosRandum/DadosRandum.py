3dadcd0ca1f8bade184f8a292c5e401c3eef5720
Joel Roberto Chan Mendez <120886127+JoelChanMendez@users.noreply.github.com>
2023-09-09 18:16:11 -0600

    Add files via upload

--- Dados/DadosRandum/DadosRandum/DadosRandum.py
+++ Dados/DadosRandum/DadosRandum/DadosRandum.py
@@ -1,10 +1,10 @@
 import random
 
 def tirar_dado():
-    # Genera un número aleatorio entre 1 y 6 (como un dado de seis caras)
+    
     resultado = random.randint(1, 6)
     return resultado
 
-# Lanza el dado y muestra el resultado
+
 resultado_dado = tirar_dado()
 print(f"El resultado del lanzamiento del dado es: {resultado_dado}")

6fd8d1910000aac59bfb6475537d7c89546456ef
Joel Roberto Chan Mendez <120886127+JoelChanMendez@users.noreply.github.com>
2023-09-30 11:02:56 -0600

    Add files via upload

---
+++ CalculoPi/CalculoDePi/CalculoDePi/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese el número de puntos a lanzar: ");
+        int numPuntos = int.Parse(Console.ReadLine());
+
+        int dentroDelCirculo = 0;
+        Random random = new Random();
+
+        for (int i = 0; i < numPuntos; i++)
+        {
+            double x = random.NextDouble(); // Genera un número aleatorio entre 0 y 1
+            double y = random.NextDouble();
+
+            double distanciaAlOrigen = Math.Pow(x, 2) + Math.Pow(y, 2);
+
+            if (distanciaAlOrigen <= 1)
+            {
+                dentroDelCirculo++;
+            }
+        }
+
+        double estimacionPi = (double)(dentroDelCirculo) / numPuntos * 4;
+        Console.WriteLine($"Estimación de Pi con {numPuntos} puntos: {estimacionPi}");
+    }
+}
+
